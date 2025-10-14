1d56ab9000d0d45ee0cb52ccf57ce4ebbe2be85f
scavallari <scavallari@nviia.com>
2025-10-14 03:52:49 -0700

    fix conditioner downloads

--- src/flux/modules/conditioner.py
+++ src/flux/modules/conditioner.py
@@ -5,16 +5,32 @@ from transformers import CLIPTextModel, CLIPTokenizer, T5EncoderModel, T5Tokeniz
 class HFEmbedder(nn.Module):
     def __init__(self, version: str, max_length: int, **hf_kwargs):
         super().__init__()
-        self.is_clip = version.startswith("openai")
+        self.is_clip = hf_kwargs.pop("is_clip", False)
         self.max_length = max_length
         self.output_key = "pooler_output" if self.is_clip else "last_hidden_state"
 
         if self.is_clip:
-            self.tokenizer: CLIPTokenizer = CLIPTokenizer.from_pretrained(version, max_length=max_length)
-            self.hf_module: CLIPTextModel = CLIPTextModel.from_pretrained(version, **hf_kwargs)
+            self.tokenizer: CLIPTokenizer = CLIPTokenizer.from_pretrained(
+                version,
+                subfolder="tokenizer",
+                max_length=max_length,
+            )
+            self.hf_module: CLIPTextModel = CLIPTextModel.from_pretrained(
+                version,
+                subfolder="text_encoder",
+                **hf_kwargs,
+            )
         else:
-            self.tokenizer: T5Tokenizer = T5Tokenizer.from_pretrained(version, max_length=max_length)
-            self.hf_module: T5EncoderModel = T5EncoderModel.from_pretrained(version, **hf_kwargs)
+            self.tokenizer: T5Tokenizer = T5Tokenizer.from_pretrained(
+                version,
+                subfolder="tokenizer_2",
+                max_length=max_length,
+            )
+            self.hf_module: T5EncoderModel = T5EncoderModel.from_pretrained(
+                version,
+                subfolder="text_encoder_2",
+                **hf_kwargs,
+            )
 
         self.hf_module = self.hf_module.eval().requires_grad_(False)
 
